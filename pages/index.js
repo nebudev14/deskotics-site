c0ec9918f48a63749a477951cbe08858ddb7c818
warren yun <xxwarrenhdxx@gmail.com>
2021-09-18 23:23:45 -0400

    Create nav component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Nav from '../components/Nav';
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <img src="/deskotics_title.png" />
+      <h1 className={styles.text}>A Bronx Science Club!</h1>
     </div>
   )
 }
