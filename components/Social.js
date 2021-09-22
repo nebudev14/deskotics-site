440c00ebb5e3ab768906a89e608cf3824a3fd75b
warren yun <xxwarrenhdxx@gmail.com>
2021-09-21 22:25:55 -0400

    Finish links

--- components/Social.js
+++ components/Social.js
@@ -10,7 +10,9 @@ const Social = () => {
                     </a>
                 </li>
                 <li>
-                    <img src="/social/instagram.svg" className={socialStyles.logo} />
+                    <a href="https://www.instagram.com/deskotics/" target="_blank">
+                        <img src="/social/instagram.svg" className={socialStyles.logo} />
+                    </a>        
                 </li>
                 <li>
                     <a href="https://drive.google.com/drive/folders/15VD3BG1y6ub6iEmi7DLK-pQNjArtF53S?usp=sharing" target="_blank">
@@ -23,7 +25,9 @@ const Social = () => {
                     </a>
                 </li>
                 <li>
-                    <img src="/social/youtube.svg" className={socialStyles.logo} />
+                    <a href="https://www.youtube.com/channel/UCH5zZ-3z-WZGAZsFpWi2bTg?view_as=subscriber" target="_blank">
+                        <img src="/social/youtube.svg" className={socialStyles.logo} />
+                    </a>
                 </li>
             </ul>
         </div>
