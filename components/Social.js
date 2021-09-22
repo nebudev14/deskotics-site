b4947310150b3dd03a032b99e52f5d856e080c51
warren yun <xxwarrenhdxx@gmail.com>
2021-09-21 21:46:17 -0400

    Social media

--- components/Social.js
+++ components/Social.js
@@ -3,7 +3,23 @@ import socialStyles from '../styles/Social.module.css';
 const Social = () => {
     return (
         <div>
-        
+            <ul className={socialStyles.socialList}>
+                <li>
+                    <img src="/social/github.svg" className={socialStyles.logo} />
+                </li>
+                <li>
+                    <img src="/social/instagram.svg" className={socialStyles.logo} />
+                </li>
+                <li>
+                    <img src="/social/drive.svg" className={socialStyles.logo} />
+                </li>
+                <li>
+                    <img src="/social/discord.svg" className={socialStyles.logo} />
+                </li>
+                <li>
+                    <img src="/social/youtube.svg" className={socialStyles.logo} />
+                </li>
+            </ul>
         </div>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Social from '../components/Social';
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -6,10 +8,10 @@ export default function Home() {
       <img src="deskotics_title.png" className={styles.title} /> 
       <h1 className={styles.text}>
         A <span className="primary">Bronx Science</span> <span className="blue">club!</span>
-      </h1>
-      <h1 className={styles.text}>
+        <br />
         <span className="blue">Tuesdays</span> in <span className="primary">Room 031</span>
       </h1>
+      <Social />
     </div>
   );
 }
