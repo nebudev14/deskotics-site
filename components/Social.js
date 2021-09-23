00b2ba48a0c4f65b8beace46f218de6a45b0772d
warren yun <xxwarrenhdxx@gmail.com>
2021-09-22 22:27:36 -0400

    Animations + fix build error

--- components/Social.js
+++ components/Social.js
@@ -5,27 +5,27 @@ const Social = () => {
         <div>
             <ul className={socialStyles.socialList}>
                 <li>                    
-                    <a href="https://github.com/deskotics" target="_blank">
+                    <a href="https://github.com/deskotics" target="_blank" rel="noreferrer">
                         <img src="/social/github.svg" className={socialStyles.logo} />                
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.instagram.com/deskotics/" target="_blank">
+                    <a href="https://www.instagram.com/deskotics/" target="_blank" rel="noreferrer">
                         <img src="/social/instagram.svg" className={socialStyles.logo} />
                     </a>        
                 </li>
                 <li>
-                    <a href="https://drive.google.com/drive/folders/15VD3BG1y6ub6iEmi7DLK-pQNjArtF53S?usp=sharing" target="_blank">
+                    <a href="https://drive.google.com/drive/folders/15VD3BG1y6ub6iEmi7DLK-pQNjArtF53S?usp=sharing" target="_blank" rel="noreferrer">
                         <img src="/social/drive.svg" className={socialStyles.logo} />
                     </a>
                 </li>
                 <li>
-                    <a href="https://discord.com/invite/GWCNzNMJDz" target="_blank">
+                    <a href="https://discord.com/invite/GWCNzNMJDz" target="_blank" rel="noreferrer">
                         <img src="/social/discord.svg" className={socialStyles.logo} />
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.youtube.com/channel/UCH5zZ-3z-WZGAZsFpWi2bTg?view_as=subscriber" target="_blank">
+                    <a href="https://www.youtube.com/channel/UCH5zZ-3z-WZGAZsFpWi2bTg?view_as=subscriber" target="_blank" rel="noreferrer">
                         <img src="/social/youtube.svg" className={socialStyles.logo} />
                     </a>
                 </li>
