7deb662ab9b60945072f671aace211fd70bfae89
warren yun <xxwarrenhdxx@gmail.com>
2021-10-01 19:00:55 -0400

    Padding issue

--- components/About.js
+++ components/About.js
@@ -2,8 +2,12 @@ import aboutStyles from '../styles/About.module.css';
 
 const About = () => {
     return (
-        <div>
-        
+        <div className={aboutStyles.container}>
+            <div className={aboutStyles.left}>
+            </div>
+            <div className={aboutStyles.right}>
+            
+            </div>
         </div>
     );
 }
