47cf1c21ab0f8f9a437791b2f9de2789da1fac43
warren yun <xxwarrenhdxx@gmail.com>
2021-10-03 22:13:00 -0400

    Scroll bar

--- components/About.js
+++ components/About.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div className={aboutStyles.container}>
             <div className={aboutStyles.left}>
+                <h1>hello</h1>
             </div>
             <div className={aboutStyles.right}>
             
--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,16 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <img src="deskotics_title.png" className={styles.title} /> 
-      <h1 className={styles.text}>
-        A <span className="primary">Bronx Science</span> <span className="blue">club!</span>
-        <br />
-        <span className="blue">Tuesdays</span> in <span className="primary">Room 031</span>
-      </h1>
-      <Social />
+    <div>
+      <div className={styles.container}>
+        <img src="deskotics_title.png" className={styles.title} /> 
+        <h1 className={styles.text}>
+          A <span className="primary">Bronx Science</span> <span className="blue">club!</span>
+          <br />
+          <span className="blue">Tuesdays</span> in <span className="primary">Room 031</span>
+        </h1>
+        <Social />
+      </div>
       <About />
     </div>
   );
