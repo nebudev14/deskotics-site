b045da12a5dbe05100698bc6a84a0c414f9cf1e5
warren yun <xxwarrenhdxx@gmail.com>
2021-10-04 22:14:54 -0400

    Format about section

--- components/About.js
+++ components/About.js
@@ -4,7 +4,10 @@ const About = () => {
     return (
         <div className={aboutStyles.container}>
             <div className={aboutStyles.left}>
-                <h1>About</h1>
+                <h1 className={aboutStyles.title}>About</h1>
+                <h2 className={aboutStyles.desc}>
+                    What exactly IS deskotics?
+                </h2>
             </div>
             <div className={aboutStyles.right}>
                 <img src="/decari/soon.png" className={aboutStyles.decOne} />
