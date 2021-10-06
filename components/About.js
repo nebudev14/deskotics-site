b58fe9c3eb13636f88e9dc0d0ae127a00ab05c35
warren yun <xxwarrenhdxx@gmail.com>
2021-10-05 22:21:11 -0400

    More content to about

--- components/About.js
+++ components/About.js
@@ -4,10 +4,13 @@ const About = () => {
     return (
         <div className={aboutStyles.container}>
             <div className={aboutStyles.left}>
-                <h1 className={aboutStyles.title}>About</h1>
-                <h2 className={aboutStyles.desc}>
-                    What exactly IS deskotics?
+                <h1 className={`${aboutStyles.title} primary`}>About</h1>
+                <h2 className={aboutStyles.titleDesc}>
+                    What exactly <span className="blue">IS</span> <span className="primary">deskotics?</span>
                 </h2>
+                <h3 className={aboutStyles.desc}>
+                    We're a Bronx Science club that loves building fun robots! We teach and build projects with circuits, programming, and cadding! No prior experience required!
+                </h3>
             </div>
             <div className={aboutStyles.right}>
                 <img src="/decari/soon.png" className={aboutStyles.decOne} />
